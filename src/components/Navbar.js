940b4fc60ac9ccf915edd44c8caacf62f836eea9
Alaa Alser <alaalsser@gmail.com>
2021-10-22 09:16:20 +0300

    nested context

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,27 +3,33 @@ import { AuthContext } from '../context/AuthContext';
 import { ThemeContext } from '../context/ThemeContext';
 
 class Navbar extends Component {
-    static contextType = AuthContext
+  static contextType = AuthContext;
   render() {
-      const {isAuthenticated,toggleAuth} = this.context
     return (
-      <ThemeContext.Consumer>
-        {(context) => {
-          const { isLightTheme, light, dark } = context;
-          const theme = isLightTheme ? light : dark;
-          return (
-            <nav style={{ color: theme.syntax, background: theme.ui }}>
-              <h3>Context App</h3>
-              <div onClick={toggleAuth}>{isAuthenticated?'Log in':'Log out'}</div>
-              <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-              </ul>
-            </nav>
-          );
-        }}
-      </ThemeContext.Consumer>
+      <AuthContext.Consumer>
+        {(authContext) => (
+          <ThemeContext.Consumer>
+            {(themeContext) => {
+              const { isLightTheme, light, dark } = themeContext;
+              const theme = isLightTheme ? light : dark;
+              const { isAuthenticated, toggleAuth } = authContext;
+              return (
+                <nav style={{ color: theme.syntax, background: theme.ui }}>
+                  <h3>Context App</h3>
+                  <div onClick={toggleAuth}>
+                    {isAuthenticated ? 'Log in' : 'Log out'}
+                  </div>
+                  <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                  </ul>
+                </nav>
+              );
+            }}
+          </ThemeContext.Consumer>
+        )}
+      </AuthContext.Consumer>
     );
   }
 }
